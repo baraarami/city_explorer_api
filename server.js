cc7a16c99909cf5c1a8dc762a5507b4edef03525
baraarami <baraaajohmani20@gmail.com>
2021-05-01 21:02:42 +0300

    lab09

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 'use strict';
 
-const express = require('express');
+const express= require('express');
+const server=express();
 require('dotenv').config();
-const cors = require('cors');
 const pg = require('pg');
-
-
-
-const server = express();
-const PORT = process.env.PORT || 3030;
-server.use(cors());
-const client = new pg.Client(process.env.DATABASE_URL);
+const cors=require('cors');
 const superagent = require('superagent');
 
+const client = new pg.Client(process.env.DATABASE_URL);
 
 
+const PORT= process.env.PORT || 5000;
+server.use(cors());
 
-server.get('/location', locationHandelr);
-server.get('/weather', weatherHandelr);
-server.get('/parks', parkHandelr);
+server.get('/', (req,res)=>{
+  res.send('<h1> Welcome To Home Page ♥ </h1>');
+});
+
+
+server.get('/location', LocationHandler);
+server.get('/weather', weatherHandler);
+server.get('/parks' , parkHandler);
+server.get('/movies', moviesHandelr);
+server.get('/yelp', yelpHandelr);
 server.get('/get', getLocationData);
-server.get('*', errorHandelr);
-
+server.get('*' , notFound);
 
 
 
@@ -52,7 +55,9 @@ function getLocationData(req, res) {
 }
 
 
-function locationHandelr(req, res) {
+
+
+function LocationHandler(req, res) {
 
   let cityName = req.query.city;
 
@@ -91,46 +96,61 @@ function locationHandelr(req, res) {
     });
 
 }
-
-function weatherHandelr(req, res) {
-
-  let cityName = req.query.search_query;
-  let key = process.env.WEATHER_KEY;
-  let weaURL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${key}&city=${cityName}&days=5`;
-
-  superagent.get(weaURL)
-    .then(weadata => {
-      let weatherData = weadata.body.data;
-
-      let allweatherdata = weatherData.map((item) => {
-
-        return new Weather(item);
-
-      });
-
-      res.send(allweatherdata);
-    })
-    .catch(error => {
-
-      res.send(error);
-    });
+function Location(cityName,data){
+  this.search_query=cityName;
+  this.formatted_query=data[0].display_name;
+  this.latitude=data[0].lat;
+  this.longitude=data[0].lon;
 
 }
 
-function parkHandelr(req, res) {
 
-  let cityName = req.query.search_query;
+
+function weatherHandler(req,res){
+  let cityName=req.query.search_query;
+  let key=process.env.WEATHER_key;
+  let weatherURL= `http://api.weatherbit.io/v2.0/forecast/daily?key=${key}&city=${cityName}&days=5`;
+
+  superagent.get(weatherURL)
+    .then(aPIData=>{
+      let weatherData= aPIData.body.data;
+      let allWeather=weatherData.map((item)=>{
+        return new Weather(item);
+      });
+      res.send(allWeather);
+    });
+}
+
+
+
+function Weather(dataW){
+  this.forecast=dataW.weather.description ;
+  this.time =convertDate(dataW.datetime);
+
+}
+
+function convertDate(d){
+  let date = new Date (d);
+  date = date.toDateString();
+  return date;
+}
+
+
+
+function parkHandler(req,res){
+
+  let cityName=req.query.search_query;
   let key = process.env.PARK_KEY;
   let parkURL = `https://developer.nps.gov/api/v1/parks?q=${cityName}&api_key=${key}`;
 
 
   superagent.get(parkURL)
-    .then(paData => {
+    .then(paData=>{
 
-      let parkData = paData.body.data;
+      let parkData=paData.body.data;
 
 
-      let parkInfo = parkData.map((item) => {
+      let parkInfo = parkData.map((item)=>{
 
         return new Park(item);
 
@@ -139,54 +159,134 @@ function parkHandelr(req, res) {
       res.send(parkInfo);
 
     })
-    .catch(error => {
+    .catch(error=>{
 
       res.send(error);
     });
 
 
 }
+function Park(cityName){
+  this.name=cityName.fullName;
+  this.address=`${cityName.addresses[0].line1}, ${cityName.addresses[0].city}, ${cityName.addresses[0].stateCode} ${cityName.addresses[0].postalCode}`;
+  this.fee='0.00';
+  this.description=cityName.description;
+  this.url=cityName.url;
 
-function errorHandelr(req, res) {
-  let errObj = {
+}
+
+function moviesHandelr(req, res) {
+
+  let countryReq = req.query.formatted_query.split(' ');
+  let country = countryReq[countryReq.length - 1];
+
+  if (country === 'USA') {
+    country = 'United States of America';
+  }
+
+
+  let key = process.env.MOVIE_API_KEY;
+  let regionURL = `https://api.themoviedb.org/3/configuration/countries?api_key=${key}`;
+  let region;
+
+
+  superagent.get(regionURL)
+    .then(data => {
+      let regionData = data.body;
+      regionData.forEach((item) => {
+        if (item.english_name === country) {
+          region = item.iso_3166_1;
+          console.log(region);
+        }
+
+      });
+      let movieURL = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&region=${region}&sort_by=popularity.desc`;
+      superagent.get(movieURL)
+        .then(data => {
+          let moviesData = data.body.results;
+          let moviesInfo = moviesData.map((item) => {
+
+            return new Movies(item);
+          });
+
+          res.send(moviesInfo);
+        });
+
+
+    })
+    .catch(error => {
+      res.send(error);
+    });
+
+}
+
+
+function yelpHandelr(req, res) {
+  let cityName = req.query.search_query;
+  let page = req.query.page;
+  let key = process.env.YELP_API_KEY;
+  const resultPerPAge = 5;
+  const start = ((page - 1) * resultPerPAge + 1);
+  let yelpURL = `https://api.yelp.com/v3/businesses/search?location=${cityName}&limit=${resultPerPAge}&offset=${start}`;
+  superagent.get(yelpURL)
+    .set('Authorization', `Bearer ${key}`)
+    .then(data => {
+
+      let yelpData = data.body.businesses;
+
+      let yelpInfo = yelpData.map((item) => {
+
+        return new Yelp(item);
+      });
+
+      res.send(yelpInfo);
+
+    })
+    .catch(error => {
+      res.send(error);
+    });
+
+
+
+
+}
+
+
+
+
+function notFound(req,res){
+  let objError = {
     status: 500,
-    resText: 'Sorry, something went wrong'
+    responseText: 'Sorry, something went wrong',
   };
-  res.status(404).send(errObj);
-
+  res.status(500).send(objError);
 }
 
 
-function convertDate(d) {
-  let date = new Date(d);
-  date = date.toDateString();
-  return date;
-}
+function Movies(moviesData) {
 
-function Weather(locData) {
-  this.forecast = locData.weather.description;
-  this.time = convertDate(locData.datetime);
-
-}
-
-function Location(cityName, locData) {
-
-  this.search_query = cityName;
-  this.formatted_query = locData[0].display_name;
-  this.latitude = locData[0].lat;
-  this.longitude = locData[0].lon;
+  this.title = moviesData.title;
+  this.overview = moviesData.overview;
+  this.average_votes = moviesData.vote_average;
+  this.total_votes = moviesData.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w500${moviesData.poster_path}`;
+  this.popularity = moviesData.popularity;
+  this.released_on = moviesData.release_date;
 
 }
 
-
-function Park(cityName) {
-  this.name = cityName.fullName;
-  this.address = `${cityName.addresses[0].line1}, ${cityName.addresses[0].city}, ${cityName.addresses[0].stateCode} ${cityName.addresses[0].postalCode}`;
-  this.fee = '0.00';
-  this.description = cityName.description;
-  this.url = cityName.url;
-
+function Yelp(yelpData){
+  this.name=yelpData.name;
+  this.image_url=yelpData.image_url;
+  this.price=yelpData.price;
+  this.rating=yelpData.rating;
+  this.url=yelpData.url;
 }
+
+
+
+
+
 
 client.connect()
   .then(() => {
